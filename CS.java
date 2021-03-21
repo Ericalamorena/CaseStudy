825b722dc8e6a0283dd93362301fec187892dd59
ericalamorena <lamorenaerica@gmail.com>
2021-03-21 18:36:01 +0800

    add Method 2 and Method 3

---
+++ CS.java
@@ -0,0 +1,20 @@
+public class CS {
+private String Address;
+private String gurdian;
+private String Gender;
+private String work;
+private int empID;
+
+ //Method  1:
+     public void getAdress(String address)
+    {
+        this.Address = address;
+    }
+    
+    public String showAddress()
+    {
+        return Address;
+    }
+    
+
+}
